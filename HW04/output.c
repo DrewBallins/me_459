4008fe0d80c100555dbb1e984fd8ea60e6b6dd65
nicmolsen <nicholas.olsen@wisc.edu>
2018-10-01 23:00:25 -0500

    Adds output function for HW04

---
+++ HW04/output.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+void output(int i) { printf("%d ", i); }
---
+++ HW04/output.h
@@ -0,0 +1,5 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+// Outputs the integer i to stdout with a trailing space
+void output(int i);
+#endif
