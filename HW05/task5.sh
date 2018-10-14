d2314487425dc376b308d4c07b11d3750f905d6b
Andrew Wild <ajwild@wisc.edu>
2018-10-14 15:14:41 -0500

    Corrected job name and some comments in task5.sh

--- HW05/task5.sh
+++ HW05/task5.sh
@@ -3,10 +3,10 @@
 # Author: Andrew Wild
 # ME 459 HW05 - task5.sh
 # This Slurm batch file compiles and runs 
-# traversal.c and related files from HW05 task3
+# traversal.c and related files from HW05 task5
 
 # Job scheduling code
-#SBATCH --job-name=HW05_task3
+#SBATCH --job-name=HW05_task5
 #SBATCH --partition=slurm_shortgpu
 #SBATCH --ntasks=1 --cpus-per-task=1
 #SBATCH --time=0-00:00:10
