6a98a911fa12becdb3ef80ba9294904be3f1e514
Andrew Wild <ajwild@wisc.edu>
2018-10-24 12:54:54 -0500

    Created task5.c program

---
+++ HW06/task5.c
@@ -0,0 +1,68 @@
+// ME 459 HW06 task5
+// Author: Andrew Wild
+// This c program function randomT2 to make a random 1000x1000 matrix A and multiplies
+// it by a length 1000 vector b to create a vector c. It then finds the norm-two of this
+// vector c and also clocks the calculation time required to run the program.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "output.h"
+
+int main() {
+	clock_t start, end;		// initialize start and end values returned by clock function
+	double cpu_time_used;	// initialize double for CPU time used
+
+	start = clock();		// clock function used to find start time
+	/* Do the work. */
+	// Allocate memory for array of doubles to make matrix A
+	unsigned int size_A = 1000000;
+	double *A = (double*) malloc(size_A * sizeof(double));
+
+	// Pass dynamically-allocated array to randomT2 function to fill it with random numbers
+	randomT2(A);
+
+	double b[1000];			// initialize array of 1000 doubles b
+	b[0] = 0.5;				// initialize first value of b
+	
+	// populate the rest of array b with values alternating from 0.5 to -0.5
+	for(int j=1; j<1000; j++) {
+		if(b[j-1] == 0.5) {
+			b[j] = -0.5;
+		}
+		else {
+			b[j] = 0.5;
+		}
+	}
+
+	double c[1000];			// create array c to store solution of A*b
+
+	// multiplying A*b column-wise and placing values in c vector
+	for(int k=0; k<1000; k++) {
+		for(int l=0; l<1000; l++) {
+			c[l] = c[l] + A[l*1000 + k]*b[k];
+		}
+	}
+
+	free(A);				// free up dynamically allocated memory for matrix A
+
+	// calculating the norm-two of vector c
+	double sum_c2 = 0;		// intializing sum of vector c^2 as a double
+	
+	for(int m=0; m<1000; m++) {
+		sum_c2 = sum_c2 + pow(c[m], 2);
+	}
+
+	double norm_c = sqrt(sum_c2);	// calculate the norm-two of vector c
+
+	end = clock();			// clock function used to find end time
+	
+	// calculate the CPU time used during the program execution
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+	// Finally, pass the norm-two of vector c and the CPU time used to outputT2 function
+	outputT2(norm_c, cpu_time_used);
+
+	return 0;
+}
