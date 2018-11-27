d711f298718430d57b90ee31aac80229342bfc51
Andrew Wild <ajwild@wisc.edu>
2018-11-27 16:44:42 -0600

    Created mesh.h header file for mesh.c file, to be used for mesh input handling

---
+++ FinalProject459/mesh.h
@@ -0,0 +1,20 @@
+/*Author: Andrew Wild*/
+#ifndef MESH_H
+#define MESH_H
+
+typedef struct triangle {
+    double x1;
+    double y1;
+    double z1;
+    double x2;
+    double y2;
+    double z2;
+    double x3;
+    double y3;
+    double z3;
+} triangle;
+
+// Reads mesh.input file and saves all triangles in array
+void read_mesh(triangle* mesh, unsigned int n);
+
+#endif
