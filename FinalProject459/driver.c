5bbaf2ff923eec7df3f31d8d9e446d98f944fda6
Andrew Wild <ajwild@wisc.edu>
2018-11-28 12:42:47 -0600

    Updated driver.c for initial testing purposes

--- FinalProject459/driver.c
+++ FinalProject459/driver.c
@@ -8,12 +8,15 @@
 int main() {
 
 	unsigned int num_t;					// initialize int for number of triangles
-	double coord;						// initialize temporary double used for coordinates
 
-	FILE *f_mesh;				// cast file
+	FILE *f_mesh;						// cast file
 	f_mesh = fopen("mesh.input","r");	// open mesh.input file for reading
 	fscanf(f_mesh, "%d", &num_t);		// Read in number of triangles
-	triangle *mesh = malloc(sizeof(triangle) * num_t)
+	triangle *mesh = malloc(sizeof(triangle) * num_t);	// dynamically allocate memory for mesh
 
+	read_mesh(mesh, f_mesh);			// call function to read mesh.input and populate mesh array of triangles
+
+	fclose(f_mesh);
+	free(mesh);
     return 0;
 }
