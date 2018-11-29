074bddf6ed0fec4cbd841d375501ac027cbe044d
Andrew Wild <ajwild@wisc.edu>
2018-11-29 15:35:34 -0600

    removed testing print statements and added comments

--- FinalProject459/driver.c
+++ FinalProject459/driver.c
@@ -11,33 +11,34 @@ int main() {
 	unsigned int num_t;					// initialize int for number of triangles
 	unsigned long int num_s;			// pointer to number of spheres
 	double r;							// radius of sphere
-
-
 	FILE *f_mesh;						// cast file mesh.input
-	FILE *f_spheres;						// cast file spheres.input
+	FILE *f_spheres;					// cast file spheres.input
+
 	f_mesh = fopen("mesh.input","r");	// open mesh.input file for reading
 	fscanf(f_mesh, "%d", &num_t);		// Read in number of triangles
 	triangle *mesh = malloc(sizeof(triangle) * num_t);	// dynamically allocate memory for mesh
 
-	read_mesh(mesh, f_mesh);			// call function to read mesh.input and populate mesh array of triangles
+	// call function to read mesh.input and populate mesh array of triangle coordinates
+	read_mesh(mesh, f_mesh);			
 
-	fclose(f_mesh);
+	fclose(f_mesh);						// close mesh.input file
 
-	f_spheres = fopen("spheres.input.csv","r");	// open spheres.input file for reading
+	// open spheres.input.csv file for reading
+	f_spheres = fopen("spheres.input.csv","r");
 
-	read_sphere_data(f_spheres, &r, &num_s);
-
-	printf("num_s = %d\n", num_s);
-	printf("radius = %f\n", r);
-
+	read_sphere_data(f_spheres, &r, &num_s);	// call function to find r & num_s from file
 	sphere *spheres = malloc(sizeof(sphere) * num_s);	// dynamically allocate memory for spheres
 
+	// call function to read spheres.input.csv and populate spheres array of sphere coordinates
 	read_spheres(spheres, f_spheres);
 
-	printf("Sphere %d coord x = %f\n", 0, spheres[0].x);
-	printf("Sphere %d coord x = %f\n", 1, spheres[1].x);
+	fclose(f_spheres);					// close spheres.input.csv file
 
-	free(mesh);
-	free(spheres);
+	/*
+	Call collision function and then output function
+	*/
+
+	free(mesh);							// free mesh dynamic memory
+	free(spheres);						// free sphere dynamic memory
     return 0;
 }
