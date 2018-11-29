26f5481a5be16e143424be60b6b65055259dd89d
Andrew Wild <ajwild@wisc.edu>
2018-11-29 15:13:25 -0600

    Made changes to call both new sphere input functions for testing

--- FinalProject459/driver.c
+++ FinalProject459/driver.c
@@ -4,12 +4,17 @@
 #include <time.h>
 
 #include "mesh.h"
+#include "spheres.h"
 
 int main() {
 
 	unsigned int num_t;					// initialize int for number of triangles
+	unsigned long int num_s;			// pointer to number of spheres
+	double r;							// radius of sphere
 
-	FILE *f_mesh;						// cast file
+
+	FILE *f_mesh;						// cast file mesh.input
+	FILE *f_spheres;						// cast file spheres.input
 	f_mesh = fopen("mesh.input","r");	// open mesh.input file for reading
 	fscanf(f_mesh, "%d", &num_t);		// Read in number of triangles
 	triangle *mesh = malloc(sizeof(triangle) * num_t);	// dynamically allocate memory for mesh
@@ -18,25 +23,21 @@ int main() {
 
 	fclose(f_mesh);
 
-	printf("Triangle 1 x1 = %f\n", mesh[0].x1);
-	printf("Triangle 1 y1 = %f\n", mesh[0].y1);
-	printf("Triangle 1 z1 = %f\n", mesh[0].z1);
-	printf("Triangle 1 x2 = %f\n", mesh[0].x2);
-	printf("Triangle 1 y2 = %f\n", mesh[0].y2);
-	printf("Triangle 1 z2 = %f\n", mesh[0].z2);
-	printf("Triangle 1 x3 = %f\n", mesh[0].x3);
-	printf("Triangle 1 y3 = %f\n", mesh[0].y3);
-	printf("Triangle 1 z3 = %f\n", mesh[0].z3);
-	printf("Triangle 1 x1 = %f\n", mesh[1].x1);
-	printf("Triangle 1 y1 = %f\n", mesh[1].y1);
-	printf("Triangle 1 z1 = %f\n", mesh[1].z1);
-	printf("Triangle 1 x2 = %f\n", mesh[1].x2);
-	printf("Triangle 1 y2 = %f\n", mesh[1].y2);
-	printf("Triangle 1 z2 = %f\n", mesh[1].z2);
-	printf("Triangle 1 x3 = %f\n", mesh[1].x3);
-	printf("Triangle 1 y3 = %f\n", mesh[1].y3);
-	printf("Triangle 1 z3 = %f\n", mesh[1].z3);
+	f_spheres = fopen("spheres.input.csv","r");	// open spheres.input file for reading
+
+	read_sphere_data(f_spheres, &r, &num_s);
+
+	printf("num_s = %d\n", num_s);
+	printf("radius = %f\n", r);
+
+	sphere *spheres = malloc(sizeof(sphere) * num_s);	// dynamically allocate memory for spheres
+
+	read_spheres(spheres, f_spheres);
+
+	printf("Sphere %d coord x = %f\n", 0, spheres[0].x);
+	printf("Sphere %d coord x = %f\n", 1, spheres[1].x);
 
 	free(mesh);
+	free(spheres);
     return 0;
 }
