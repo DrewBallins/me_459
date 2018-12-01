4058d2413fdb16cf607c8090da06c4578d499da7
Andrew Wild <ajwild@wisc.edu>
2018-12-01 09:47:24 -0600

    Created collide.h header file to define collision function
    collide.h header file defines collide_all function which returns the number of collisions between spheres and triangles in the mesh

---
+++ FinalProject459/collide.h
@@ -0,0 +1,13 @@
+/*Author: Andrew Wild*/
+#ifndef COLLIDE_H
+#define COLLIDE_H
+
+#include "spheres.h"
+#include "mesh.h"
+
+// Returns the number of distinct pairs of collisions between distinct spheres 
+// (all of radius r) and distinct triangles in the mesh
+// ie Returns |{ {i,j}: i != j && sphere_i has non-negative overlap with triangle_i}|
+unsigned int collide_all_spheres(double r, sphere* spheres, triangle* mesh, unsigned long int num_s, size_t num_t);
+
+#endif
