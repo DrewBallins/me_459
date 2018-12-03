f9520ca6a1827af68eb4fa194f246389c1ce606f
Andrew Wild <ajwild@wisc.edu>
2018-12-02 21:21:53 -0600

    Changed both task1 files to use C std library
    As was requested by Nic in the piazza forum, I changed both my task1a.c and my task1b.c files to use the C standard library functions INT_MAX and INT_MIN rather than calculating them myself

--- HW10/task1a.c
+++ HW10/task1a.c
@@ -3,11 +3,11 @@
    Due: 12/09/2018 */
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
+#include <limits.h>
 
 int main() {
-	int sz = pow(2, 31) - 1;	// maximum value int that can be stored
-	int sz2 = sz + 1;			// 1 value greater than max value int
+	int sz = INT_MAX;	// maximum value int that can be stored
+	int sz2 = sz + 1;	// 1 value greater than max value int
 
 	// display these two values
 	printf("Maximum value int = %d\n", sz);
