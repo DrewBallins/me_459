13a8346b35d51a093d00fd6408eea7ebe4ddf02a
Andrew Wild <ajwild@wisc.edu>
2018-12-03 18:23:15 -0600

    added new struct vector to collide.h

--- FinalProject459/collide.h
+++ FinalProject459/collide.h
@@ -5,6 +5,12 @@
 #include "spheres.h"
 #include "mesh.h"
 
+typedef struct vector {
+    double x;
+    double y;
+    double z;
+} vector;
+
 // Returns the number of distinct pairs of collisions between distinct spheres 
 // (all of radius r) and distinct triangles in the mesh
 // ie Returns |{ {i,j}: i != j && sphere_i has non-negative overlap with triangle_i}|
