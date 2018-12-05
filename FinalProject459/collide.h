265f48450a8c920178d43adb74450a83e2630849
Andrew Wild <ajwild@wisc.edu>
2018-12-05 11:35:59 -0600

    Defined pairs struct to save ID of collided s-t pairs

--- FinalProject459/collide.h
+++ FinalProject459/collide.h
@@ -11,9 +11,14 @@ typedef struct vector {
     double z;
 } vector;
 
+typedef struct pair {
+	size_t s;
+	size_t t;
+} pair;
+
 // Returns the number of distinct pairs of collisions between distinct spheres 
 // (all of radius r) and distinct triangles in the mesh
 // ie Returns |{ {i,j}: i != j && sphere_i has non-negative overlap with triangle_i}|
-size_t collide_all(double r, sphere* spheres, triangle* mesh, unsigned long int num_s, size_t num_t);
+size_t collide_all(double r, sphere* spheres, triangle* mesh, pair* pairs, unsigned long int num_s, size_t num_t);
 
 #endif
