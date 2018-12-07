de82708896a487ba92730aa0a0e9077eaef10bd5
Andrew Wild <ajwild@wisc.edu>
2018-12-07 13:48:56 -0600

    Created output header file to define output function

---
+++ FinalProject459/output.h
@@ -0,0 +1,10 @@
+/*Author: Andrew Wild*/
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#include "collide.h"
+
+// Outputs sphere-triangle collision pairs
+void data_out(pair *p, size_t n, FILE *f);
+
+#endif
