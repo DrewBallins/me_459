694ef1b380fa456ed0b16cd8233aa7dfb2fea443
Andrew Wild <ajwild@wisc.edu>
2018-12-07 13:50:03 -0600

    Created output.c file with complete data_out funciton

---
+++ FinalProject459/output.c
@@ -0,0 +1,17 @@
+/*Author: Andrew Wild*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "output.h"
+#include "collide.h"
+
+// Outputs sphere-triangle collision pairs
+void data_out(pair *p, size_t n, FILE *f) {
+	fprintf(f, "s,t\n");				// print first line of file
+
+	// print sphere and triangle ID's of all collided pairs
+	for (size_t k = 0; k < n; k++) {
+		fprintf(f, "%d,%d\n", p[k].s, p[k].t);
+	}
+
+}
